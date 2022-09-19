5e8681e4a5255fcf6f1616d899ed4c3a0d79c251
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-19 21:23:29 +0530

    Added : int dies;

--- src/com/snakeladder/game/SnakeLadderGame.java
+++ src/com/snakeladder/game/SnakeLadderGame.java
@@ -1,7 +1,13 @@
 package com.snakeladder.game;
 
 public class SnakeLadderGame {
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake Ladder Game");
+		int dies;
+		// player role die
+		dies = (int) (Math.random()*0) ;
+
+		System.out.println("the dice number is   : " + dies  );
 	}
 }
