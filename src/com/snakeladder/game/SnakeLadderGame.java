73f333168238ba6b9253a8063658981d09073a10
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-19 21:39:04 +0530

    Added : Player Play game & they are NoPlay_Ladder_Snake Condtion

--- src/com/snakeladder/game/SnakeLadderGame.java
+++ src/com/snakeladder/game/SnakeLadderGame.java
@@ -4,10 +4,34 @@ public class SnakeLadderGame {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake Ladder Game");
-		
-		int dies;
-		// result
-		dies = (int) (Math.random()*6) + 1;
-		System.out.println("the dice number is  :" + dies  );
+	
+		// Variables
+				int palyer1=0;
+				int dies;
+				int objective=0;
+				// result
+
+				dies = (int) (Math.random()*6) + 1;
+
+				System.out.println("DIES" + dies );
+				int instruction = (int) (Math.random()*3)+1;
+				System.out.println("instruction" + instruction );
+
+				switch ((int)instruction )
+				{
+				case  1 :
+					System.out.println(" NO_PLAY ");
+					objective=dies -(1 * dies) ;
+					break;
+				case 2 :
+					System.out.println(" LADDER_PLAY ");
+					objective= (1 * dies);
+					break;
+				case 3 :
+					System.out.println(" SNAK_PLAY ");
+					objective= -(1 * dies) ;
+				}
+
+				System.out.println("The value of instruction is  " + objective );
 	}
 }
